282d9f143f8ee56ab36ec17c502733a08e63910c
Chris <chrisy@udel.edu>
2024-10-08 20:48:35 -0400

    fixed merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -30,10 +30,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-add-name-chris
           Chris
           Olivia Bouvier
-main
         </p>
         <a
           className="App-link"
