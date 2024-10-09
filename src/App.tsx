7517b3a79286b3bbc5049ca3399e5d9f4a1f4ee2
Tarun <tarunb@udel.edu>
2024-10-08 20:48:14 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          Tarun Baskaran
         </p>
         <a
           className="App-link"
