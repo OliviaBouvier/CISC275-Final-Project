df977f6ada55971d6033ed1a0f02dad6cc7be1bf
Tarun <tarunb@udel.edu>
2024-10-14 12:47:57 -0400

    Added descriptions to both pages.

--- src/pages/basicQuestions.tsx
+++ src/pages/basicQuestions.tsx
@@ -2,7 +2,7 @@ export function BasicQuestions(){
     return <div>
         <h1>Basic Questions</h1>
         <p>
-            A simplified version of the quiz containing only multiple choice questions. 
+            A simplified version of the quiz containing only multiple choice questions. There is no time limit.
         </p>
         </div>
 }
--- src/pages/detailedQuestions.tsx
+++ src/pages/detailedQuestions.tsx
@@ -2,7 +2,7 @@ export function DetailedQuestions(){
     return <div> 
     <h1>Detailed Questions</h1>
     <p>
-        A more detailed version of the quiz containing more specific questions. 
+        A more detailed version of the quiz containing more specific questions. There is no time limit.
     </p>
     </div>
     
