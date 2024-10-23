a11b1e08d67f418c1aaa629cea8da85ec3877cc1
Tarun <tarunb@udel.edu>
2024-10-22 21:01:58 -0400

    Massive visual update to the basic and detailed questions pages.

--- src/pages/basicQuestions.tsx
+++ src/pages/basicQuestions.tsx
@@ -1,24 +1,63 @@
-import { ChangeQuestionOneB, ChangeQuestionTwoB, ChangeQuestionThreeB, ChangeQuestionOneFourB, ChangeQuestionOneFiveB, ChangeQuestionOneSixB, ChangeQuestionOneSevenB} from "../components/BasicQuestionRBOne";
+import { 
+    ChangeQuestionOneB, 
+    ChangeQuestionTwoB, 
+    ChangeQuestionThreeB, 
+    ChangeQuestionOneFourB, 
+    ChangeQuestionOneFiveB, 
+    ChangeQuestionOneSixB, 
+    ChangeQuestionOneSevenB 
+} from "../components/BasicQuestionRBOne";
 
+export function BasicQuestions() {
+    return (
+        <div style={{ position: 'relative', width: '80%', margin: '0 auto', textAlign: 'center' }}>
+            <h1>Basic Questions</h1>
+            <p>A simplified version of the quiz containing only multiple choice questions. There is no time limit.</p>
+            
+            {/* Container for the questions and lines */}
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '40px' }}>
+                {/* Left column of questions */}
+                <div style={{ width: '45%', textAlign: 'center' }}>
+                    <div><ChangeQuestionOneB /></div>
+                    <hr style={{ borderTop: '3px solid black', margin: '20px 0' }} />
+                    <div><ChangeQuestionThreeB /></div>
+                    <hr style={{ borderTop: '3px solid black', margin: '20px 0' }} />
+                    <div><ChangeQuestionOneFiveB /></div>
+                </div>
 
-export function BasicQuestions(){
-    return (<div>
-        <h1>Basic Questions</h1>
-        <p>
-            A simplified version of the quiz containing only multiple choice questions. 
-        </p>
-        <div><ChangeQuestionOneB></ChangeQuestionOneB></div>
-        <pre></pre>
-        <div><ChangeQuestionTwoB></ChangeQuestionTwoB></div>
-        <pre></pre>
-        <div><ChangeQuestionThreeB></ChangeQuestionThreeB></div>
-        <pre></pre>
-        <div><ChangeQuestionOneFourB></ChangeQuestionOneFourB></div>
-        <pre></pre>
-        <div><ChangeQuestionOneFiveB></ChangeQuestionOneFiveB></div>
-        <pre></pre>
-        <div><ChangeQuestionOneSixB></ChangeQuestionOneSixB></div>
-        <pre></pre>
-        <div><ChangeQuestionOneSevenB></ChangeQuestionOneSevenB></div>
-        </div>);
+                {/* Vertical Line */}
+                <div style={{ width: '10px', display: 'flex', alignItems: 'center' }}>
+                    <div
+                        style={{
+                            height: '115%',
+                            borderLeft: '3px solid grey',
+                            marginLeft: '5px',
+                        }}
+                    ></div>
+                </div>
+
+                {/* Right column of questions */}
+                <div style={{ width: '45%', textAlign: 'center' }}>
+                    <div><ChangeQuestionTwoB /></div>
+                    <hr style={{ borderTop: '3px solid black', margin: '20px 0' }} />
+                    <div><ChangeQuestionOneFourB /></div>
+                    <hr style={{ borderTop: '3px solid black', margin: '20px 0' }} />
+                    <div><ChangeQuestionOneSixB /></div>
+                </div>
+            </div>
+
+            {/* Question 7 with surrounding box */}
+            <div style={{  display: 'inline-block', padding: '20px', textAlign: 'center' }}>
+                {/* Box with thicker bottom line */}
+                <div style={{ 
+                    border: '3px solid grey', 
+                    padding: '20px', 
+                    display: 'inline-block', 
+                    width: '60%' 
+                }}>
+                    <ChangeQuestionOneSevenB />
+                </div>
+            </div>
+        </div>
+    );
 }
