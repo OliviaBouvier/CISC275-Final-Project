9a1e508060437b92f43de6750a597d412e7c76f5
Olivia Bouvier <obouvier@udel.edu>
2024-11-13 14:39:57 -0500

    Trying a HashRouter to fix 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Navbar } from 'react-bootstrap';
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Routes,
     Route
 } from "react-router-dom";
